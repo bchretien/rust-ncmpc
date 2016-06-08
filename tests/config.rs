af6e1fd05b7b649e957757149593dde42755b679
Benjamin Chrétien <chretien.b@gmail.com>
2016-06-08 15:39:08 +0200

    tests: reorganize a bit

--- tests/config.rs
+++ tests/config.rs
@@ -5,11 +5,8 @@ use std::env;
 use std::net::{IpAddr, SocketAddr};
 use std::str::FromStr;
 
-fn get_config_path(name: &str) -> PathBuf {
-  let mut path = PathBuf::from(file!());
-  path.pop();
-  path.push(name);
-  path
+fn init_logger() {
+  env_logger::init();
 }
 
 fn reset_env_vars() {
@@ -17,12 +14,25 @@ fn reset_env_vars() {
   env::remove_var("MPD_PORT");
 }
 
+fn before_each() {
+  init_logger();
+  reset_env_vars();
+}
+
+fn after_each() {}
+
+fn get_config_path(name: &str) -> PathBuf {
+  let mut path = PathBuf::from(file!());
+  path.pop();
+  path.push(name);
+  path
+}
 
 /// Test used to compare our default configuration with ncmpcpp's default
 /// configuration.
 #[test]
 fn load_default_config() {
-  reset_env_vars();
+  before_each();
 
   use ncmpc::{ConfigLoader, ParamConfig};
   let config_loader = ConfigLoader::new();
@@ -31,12 +41,14 @@ fn load_default_config() {
 
   let params = ParamConfig::new();
   assert_eq!(config.params, params);
+
+  after_each();
 }
 
 /// Test used to check custom configurations.
 #[test]
 fn load_custom_config() {
-  reset_env_vars();
+  before_each();
 
   use ncmpc::{ConfigLoader, ParamConfig};
   let config_loader = ConfigLoader::new();
@@ -46,11 +58,15 @@ fn load_custom_config() {
   let mut params = ParamConfig::new();
   params.mpd_port = 7700;
   assert_eq!(config.params, params);
+
+  after_each();
 }
 
 /// Test MPD socket address.
 #[test]
 fn mpd_socket_addr() {
+  before_each();
+
   use ncmpc::{ConfigLoader, ParamConfig};
   let config_loader = ConfigLoader::new();
   let config_path = get_config_path("custom_config");
@@ -59,4 +75,6 @@ fn mpd_socket_addr() {
   let params = ParamConfig::new();
   let addr: SocketAddr = "127.0.0.1:7700".parse().unwrap();
   assert_eq!(config.socket_addr(), addr);
+
+  after_each();
 }
